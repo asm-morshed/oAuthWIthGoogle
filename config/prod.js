05acd56bb75e4fb6a7d20f376fd2cfff0b2bc261
asm-morshed <mm.morshed28707@gmail.com>
2018-08-09 01:04:45 +0600

    modification for strategy setting

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 // prod.js - production keys here
 
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID, //'651162464687-bhrqrjvtcfneqe1dfb46u1frjfgcqndf.apps.googleusercontent.com',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,//5-OY_zf6c0U7TzCfE3LyuO-g',
-    mongoURI: process.env.MONGO_URI,//'mongodb://asmmorshed:asmmorshed5@ds115442.mlab.com:15442/email-dev',
-    cookieKey: process.env.COOKIE_KEY//'sdjflksdjflkjsfkljsdkfjskdjflskjflkjsdfklj'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 }
